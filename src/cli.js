16672c8c4f18766d9f13a1a691acf6812ed30814
leksandrFo <leksandr.fo@gmail.com>
2022-11-27 13:17:29 +0300

    fix based on eslint

--- src/cli.js
+++ src/cli.js
@@ -1,10 +1,9 @@
 import readlineSync from 'readline-sync';
 
 export default () => {
-    let playerName = readlineSync.question('May I have your name? ');
-    while (playerName === '') {
-        playerName = readlineSync.question('Excuse me, could you tell me your name again? ');
-    }
-    console.log(`Hello, ${playerName}!`);
+  let playerName = readlineSync.question('May I have your name? ');
+  while (playerName === '') {
+    playerName = readlineSync.question('Excuse me, could you tell me your name again? ');
+  }
+  console.log(`Hello, ${playerName}!`);
 };
-
