e6e0dc372187306633fc110b2c4b5460c7a8157a
leksandrFo <leksandr.fo@gmail.com>
2022-12-17 20:41:16 +0300

    add a new folder "games"

---
+++ src/games/calculator.js
@@ -0,0 +1,69 @@
+import generateRandomNumber from '../generateRandomNumber.js';
+import startGame from '../index.js';
+
+const description = 'What is the result of the expression?';
+const mathSymbols = ['+', '-', '*'];
+
+const generateRandomMathSymbol = () => {
+  return mathSymbols[Math.floor(Math.random() * mathSymbols.length)];
+};
+
+// Функция подсчета результата выражения
+const resultOfExpression = (question) => {
+  const arrFromExpression = question.split(' ');
+  const signOfExpression = arrFromExpression[1];
+  let result = 0;
+  switch (signOfExpression) {
+  case mathSymbols[0]:
+    result = Number(arrFromExpression[0]) + Number(arrFromExpression[2]);
+    break;
+  case mathSymbols[1]:
+    result = Number(arrFromExpression[0]) - Number(arrFromExpression[2]);
+    break;
+  case mathSymbols[2]:
+    result = Number(arrFromExpression[0]) * Number(arrFromExpression[2]);
+    break;
+  }
+  return result;
+};
+
+// Функция аккумулирования генерации выражения и ответа
+const questionAndAnswer = () => {
+  const signOfExpression = generateRandomMathSymbol();
+  
+  const number = {};
+  number.first = generateRandomNumber(0, 100);
+  number.second = generateRandomNumber(0, 100);
+  
+  let question = `${number.first} ${signOfExpression} ${number.second}`;
+  
+  if (signOfExpression === '-') {// Условия на вычитание
+    if (number.first === number.second || number.second > number.first) {
+    // Первое число не должно быть равно нулю и второму числу, а также второе число должно быть меньше первого, чтобы не уходить в отрицательные числа
+        while (number.first === number.second || number.second > number.first) {
+          number.first = generateRandomNumber(1, 100);
+          number.second = generateRandomNumber(0, 100);
+      }
+      question = `${number.first} ${signOfExpression} ${number.second}`
+    }
+  }
+  if (signOfExpression === '*') {
+    if (number.first > 10 && number.second > 10) {
+    // Первое число не должно быть равно нулю, а также одно из чисел не должно быть больше 10 для простоты подсчета
+        while (number.first === 0 || number.first > 10 && number.second > 10) {
+          number.first = generateRandomNumber(1, 100);
+          number.second = generateRandomNumber(0, 100);
+      }
+      question = `${number.first} ${signOfExpression} ${number.second}`;
+    }
+  }
+  const answer = String(resultOfExpression(question));
+  return {question, answer};
+};
+
+// Функция калькулятора
+const calculator = () => {
+  startGame(description, questionAndAnswer);
+};
+
+export default calculator;
---
+++ src/games/isEven.js
@@ -0,0 +1,16 @@
+import generateRandomNumber from '../generateRandomNumber.js';
+import startGame from '../index.js';
+
+const description = 'Answer "yes" if the number is even, otherwise answer "no".';
+
+const questionAndAnswer = () => {
+  const question = generateRandomNumber(0, 100);
+  const answer = question % 2 === 0 ? 'yes' : 'no';
+  return {question, answer};
+};
+
+const isEven = () => {
+  startGame(description, questionAndAnswer);
+};
+
+export default isEven;
