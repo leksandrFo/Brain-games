f2e510dca3c95894e4bb31c6fa0083c2f3d7e6ef
leksandrFo <leksandr.fo@gmail.com>
2022-12-25 16:18:47 +0300

    add capitalize function

--- src/cli.js
+++ src/cli.js
@@ -2,8 +2,10 @@ import readlineSync from 'readline-sync';
 
 const player = {};
 
+const capitalize = (text) => text[0].toUpperCase() + text.slice(1);
+
 const playerName = () => {
-  player.name = readlineSync.question('May I have your name? ');
+  player.name = capitalize(readlineSync.question('May I have your name? '));
   while (player.name === '') {
     player.name = readlineSync.question('Excuse me, could you tell me your name again? ');
   }
