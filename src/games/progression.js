a641a0ebcc5df72e9d969e7e9d80d4d7e619df9d
leksandrFo <leksandr.fo@gmail.com>
2022-12-28 16:22:43 +0300

    fix const progressionLength

--- src/games/progression.js
+++ src/games/progression.js
@@ -7,8 +7,8 @@ const getQuestionAndAnswer = () => {
   const firstNumber = generateRandomNumber(0, 100);
   const progressionArray = [firstNumber];
   const progressionStep = generateRandomNumber(1, 10);
-  const progressionLength = generateRandomNumber(4, 10);
-  for (let i = 0; i < progressionLength; i += 1) {
+  const progressionLength = generateRandomNumber(5, 10);
+  for (let i = 0; i < progressionLength - 1; i += 1) {
     progressionArray.push(progressionArray[i] + progressionStep);
   }
   const randomIndex = generateRandomNumber(0, progressionArray.length - 1);
